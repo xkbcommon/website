658c854bdc97786d31f64ddf9ef153db71b18258
Pierre Le Marre <dev@wismill.eu>
2025-10-20 19:03:10 +0200

    1.12.2

---
+++ doc/1.12.2/search/all_4.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['4_20–_202019_2002_2022_0',['libxkbcommon 0.8.4 – 2019-02-22',['../release-notes.html#libxkbcommon-084--2019-02-22',1,'']]],
+  ['4_200_20–_202014_2002_2002_1',['libxkbcommon 0.4.0 – 2014-02-02',['../release-notes.html#libxkbcommon-040--2014-02-02',1,'']]],
+  ['4_200_20–_202022_2002_2004_2',['libxkbcommon 1.4.0 – 2022-02-04',['../release-notes.html#libxkbcommon-140--2022-02-04',1,'']]],
+  ['4_201_20–_202014_2003_2027_3',['libxkbcommon 0.4.1 – 2014-03-27',['../release-notes.html#libxkbcommon-041--2014-03-27',1,'']]],
+  ['4_201_20–_202022_2005_2021_4',['libxkbcommon 1.4.1 – 2022-05-21',['../release-notes.html#libxkbcommon-141--2022-05-21',1,'']]],
+  ['4_202_20–_202014_2005_2015_5',['libxkbcommon 0.4.2 – 2014-05-15',['../release-notes.html#libxkbcommon-042--2014-05-15',1,'']]],
+  ['4_203_20–_202014_2008_2019_6',['libxkbcommon 0.4.3 – 2014-08-19',['../release-notes.html#libxkbcommon-043--2014-08-19',1,'']]],
+  ['407_20–_20conflicting_20key_20type_20definitions_7',['XKB-407 – Conflicting key type definitions',['../error-index.html#XKB-407',1,'']]],
+  ['428_20–_20global_20defaults_20wrong_20scope_8',['XKB-428 – Global defaults wrong scope',['../error-index.html#XKB-428',1,'']]],
+  ['433_20–_20missing_20default_20section_9',['XKB-433 – Missing default section',['../error-index.html#XKB-433',1,'']]],
+  ['461_20–_20conflicting_20key_20symbol_10',['XKB-461 – Conflicting key symbol',['../error-index.html#XKB-461',1,'']]],
+  ['478_20–_20invalid_20operation_11',['XKB-478 – Invalid operation',['../error-index.html#XKB-478',1,'']]],
+  ['489_20–_20numeric_20keysym_12',['XKB-489 – Numeric keysym',['../error-index.html#XKB-489',1,'']]]
+];
---
+++ doc/1.12.2/search/all_f.js
@@ -0,0 +1,24 @@
+var searchData=
+[
+  ['failed_0',['XKB-822 – Keymap compilation failed',['../error-index.html#XKB-822',1,'']]],
+  ['faq_1',['Frequently Asked Question (FAQ)',['../faq.html',1,'']]],
+  ['features_2',['features',['../xkbcommon-compatibility.html#general-features',1,'General features'],['../todo.html#_todo000003',1,'General features']]],
+  ['field_3',['field',['../error-index.html#XKB-563',1,'XKB-563 – Invalid action field'],['../error-index.html#XKB-639',1,'XKB-639 – Unknown default field'],['../error-index.html#XKB-711',1,'XKB-711 – Unsupported symbols field'],['../error-index.html#XKB-812',1,'XKB-812 – Unknown field']]],
+  ['field_20type_4',['XKB-578 – Wrong field type',['../error-index.html#XKB-578',1,'']]],
+  ['fields_5',['XKB-935 – Conflicting key fields',['../error-index.html#XKB-935',1,'']]],
+  ['file_6',['file',['../rule-file-format.html#format-of-the-file',1,'Format of the file'],['../rule-file-format.html',1,'The rules file'],['../keymap-text-format-v1-v2.html#xkb-file',1,'XKB file'],['../error-index.html#XKB-661',1,'XKB-661 – Invalid included file']]],
+  ['file_20encoding_7',['XKB-542 – Invalid file encoding',['../error-index.html#XKB-542',1,'']]],
+  ['file_20names_8',['Using system file names',['../user-configuration.html#user-config-system-file-names',1,'']]],
+  ['file_20not_20found_9',['XKB-338 – Included file not found',['../error-index.html#XKB-338',1,'']]],
+  ['fixes_10',['Fixes',['../release-notes.html#fixes',1,'Fixes'],['../release-notes.html#fixes-1',1,'Fixes'],['../release-notes.html#fixes-2',1,'Fixes'],['../release-notes.html#fixes-3',1,'Fixes'],['../release-notes.html#fixes-4',1,'Fixes'],['../release-notes.html#fixes-5',1,'Fixes'],['../release-notes.html#fixes-6',1,'Fixes'],['../release-notes.html#fixes-7',1,'Fixes'],['../release-notes.html#fixes-8',1,'Fixes'],['../release-notes.html#fixes-9',1,'Fixes'],['../release-notes.html#fixes-10',1,'Fixes'],['../release-notes.html#fixes-11',1,'Fixes'],['../release-notes.html#fixes-12',1,'Fixes'],['../release-notes.html#fixes-13',1,'Fixes'],['../release-notes.html#fixes-14',1,'Fixes'],['../release-notes.html#fixes-15',1,'Fixes']]],
+  ['flags_11',['flags',['../keymap-text-format-v1-v2.html#generic-flags',1,'Generic flags'],['../keymap-text-format-v1-v2.html#section-flags-def',1,'Section flags'],['../keymap-text-format-v1-v2.html#symbols-flags',1,'Symbols flags']]],
+  ['flags_20actions_12',['Control flags actions',['../keymap-text-format-v1-v2.html#control-flags-actions',1,'']]],
+  ['for_20available_20rmlvo_13',['Query for available RMLVO',['../group__registry.html',1,'']]],
+  ['for_20common_20modifiers_20and_20leds_14',['Predefined names for common modifiers and LEDs',['../group__modifier-names.html',1,'']]],
+  ['format_15',['format',['../keymap-text-format-v1-v2.html#introduction-to-the-xkb-text-format',1,'Introduction to the XKB text format'],['../xkbcommon-compatibility.html#keymap-text-format',1,'Keymap text format'],['../todo.html#_todo000004',1,'Keymap text format'],['../release-notes.html#keymap-text-format-1',1,'Keymap text format'],['../release-notes.html#rules-text-format',1,'Rules text format'],['../xkb-intro.html#xkb-the-text-format',1,'XKB the text format'],['../todo.html#_todo000001',1,'XKB the text format'],['../error-index.html#XKB-742',1,'XKB-742 – Incompatible keymap text format']]],
+  ['format_20of_20the_20file_16',['Format of the file',['../rule-file-format.html#format-of-the-file',1,'']]],
+  ['format_20v1_20and_20v2_17',['The XKB keymap text format, V1 and V2',['../keymap-text-format-v1-v2.html',1,'']]],
+  ['found_18',['XKB-338 – Included file not found',['../error-index.html#XKB-338',1,'']]],
+  ['four_20levels_19',['Four levels',['../keymap-text-format-v1-v2.html#four-levels',1,'']]],
+  ['frequently_20asked_20question_20faq_20',['Frequently Asked Question (FAQ)',['../faq.html',1,'']]]
+];
